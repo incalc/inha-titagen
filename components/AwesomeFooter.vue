85ea5055ae21ca63dd84d6681308b3c7762902b0
Seungjae Park <astro.psj@gmail.com>
2019-05-05 00:19:20 +0900

    Improve the footer component

--- components/AwesomeFooter.vue
+++ components/AwesomeFooter.vue
@@ -3,8 +3,14 @@
     <v-card class="flex" flat tile>
       <v-card-title class="primary">
         <strong class="subheading">Inha Univ. Time Table Generator</strong>
+        <v-spacer/>
+        <a class="view-on-github" href="https://github.com/Astro36/inha-timetable-generator">
+          View on <span class="github">GitHub</span>
+        </a>
       </v-card-title>
-      <v-card-actions class="grey darken-3 justify-center">&copy;2019 — Seungjae Park</v-card-actions>
+      <v-card-actions
+        class="grey darken-3 justify-center"
+      >&copy; 2019 Seungjae Park. All rights reserved.</v-card-actions>
     </v-card>
   </v-footer>
 </template>
@@ -14,3 +20,14 @@ export default {
   name: 'AwesomeFooter'
 }
 </script>
+
+<style scoped>
+.view-on-github {
+  color: white;
+  text-decoration: none;
+}
+
+.github {
+  font-weight: 700;
+}
+</style>

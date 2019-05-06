667ac40bfce70036e3f75670756596a4f8a711d1
Seungjae Park <astro.psj@gmail.com>
2019-05-06 19:10:49 +0900

    Update docs

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@ import VuetifyLoaderPlugin from 'vuetify-loader/lib/plugin'
 import pkg from './package'
 
 export default {
-  mode: 'universal',
+  mode: 'spa',
 
   /*
   ** Headers of the page
@@ -50,44 +50,45 @@ export default {
   manifest: {
     name: 'Inha Timetable',
     short_name: 'Inha Timetable',
-    start_url: './?utm_source=homescreen',
+    scope: '/inha-timetable-generator/',
+    start_url: '/inha-timetable-generator/?utm_source=homescreen',
     display: 'standalone',
     background_color: '#005bac',
     theme_color: '#005bac',
     description: 'Inha Univ. Timetable Generator',
     icons: [
       {
-        src: '../touch/icon48.png',
+        src: '/inha-timetable-generator/touch/icon48.png',
         sizes: '48x48',
         type: 'image/png'
       },
       {
-        src: '../touch/icon72.png',
+        src: '/inha-timetable-generator/touch/icon72.png',
         sizes: '72x72',
         type: 'image/png'
       },
       {
-        src: '../touch/icon96.png',
+        src: '/inha-timetable-generator/touch/icon96.png',
         sizes: '96x96',
         type: 'image/png'
       },
       {
-        src: '../touch/icon144.png',
+        src: '/inha-timetable-generator/touch/icon144.png',
         sizes: '144x144',
         type: 'image/png'
       },
       {
-        src: '../touch/icon168.png',
+        src: '/inha-timetable-generator/touch/icon168.png',
         sizes: '168x168',
         type: 'image/png'
       },
       {
-        src: '../touch/icon192.png',
+        src: '/inha-timetable-generator/touch/icon192.png',
         sizes: '192x192',
         type: 'image/png'
       },
       {
-        src: '../touch/icon512.png',
+        src: '/inha-timetable-generator/touch/icon512.png',
         sizes: '512x512',
         type: 'image/png'
       }
